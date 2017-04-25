fddc248333b85f2ba333141bf30f933785fd7ee7
hoffa2 <helhof@online.no>
2017-04-25 13:00:59 +0200

    chord: added more chord logic

--- segment/chord.go
+++ segment/chord.go
@@ -1,28 +1,63 @@
 package segment
 
-import "context"
+import (
+	"context"
+	"io"
+	"os"
 
-type chord struct {
+	"crypto/sha1"
+
+	"github.com/hoffa2/worm/protobuf/chord"
+)
+
+type Node interface {
+	Shutdown()
 }
 
-func (c *chord) Alive(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
+type Node struct {
+	*chord.Node
+	Node
+}
+
+func hashValue(str string) string {
+	h := sha1.New()
+	io.WriteString(h, str)
+	return string(h.Sum(nil))
+}
+
+func SetupKNode(addr, port string, nodeinterface Node) (*Kademlia, error) {
+	h, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+	n := &chord.Node{
+		ID:        hashValue(h),
+		IpAddress: addr,
+		RpcPort:   port,
+	}
+	return &Kademlia{Node: n}
+}
+
+func (n *Node) Alive(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
+	return &chord.Alive{IsAlive: true}, nil
+}
+
+func (n *Node) FindPredecessor(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
 	return nil, nil
 }
 
-func (c *chord) FindPredecessor(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
+func (n *Node) Init(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
 	return nil, nil
 }
 
-func (c *chord) Init(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
+func (n *Node) Notify(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
 	return nil, nil
 }
 
-func (c *chord) Notify(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
-	return nil, nil
-
+func (n *Node) Shutdown(ctx context.Context, empty *chord.Empty) (*chord.Empty, error) {
+	k.node.Shutdown()
 }
 
-func (c *chord) Shutdown(ctx context.Context, empty *chord.Empty) (*chord.Alive, error) {
-	return nil, nil
 
-}
+func
+
